28b6f3c96319c00fc2285f1b9f6e9fc2df8485aa
Nhan Nguyen <48123439+edwardnguyen225@users.noreply.github.com>
2024-08-19 13:37:18 +0700

    feat: Replace with fully working handlers

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,8 +1,37 @@
-import { APIGatewayProxyEvent, APIGatewayProxyEventQueryStringParameters, APIGatewayProxyResult } from "aws-lambda";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyEventQueryStringParameters,
+  APIGatewayProxyResult,
+} from "aws-lambda";
 import AWS, { AWSError } from "aws-sdk";
-import { CLIENTS_TABLE_NAME } from "./constants";
-import { responseOK } from "./responses";
-import { Action, Client } from "./types";
+import { Key } from "aws-sdk/clients/dynamodb";
+import { v4 } from "uuid";
+
+type Action =
+  | "getClients"
+  | "getMessages"
+  | "sendMessage"
+  | "$connect"
+  | "$disconnect";
+
+type Client = {
+  connectionId: string;
+  nickname: string;
+};
+
+type SendMessageBody = {
+  recipientNickname: string;
+  message: string;
+};
+
+type GetMessagesBody = {
+  targetNickname: string;
+  startKey: Key | undefined;
+  limit: number;
+};
+
+const CLIENTS_TABLE_NAME = "Clients";
+const MESSAGES_TABLE_NAME = "Messages";
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -10,38 +39,78 @@ const apigw = new AWS.ApiGatewayManagementApi({
   endpoint: process.env.WSSAPIGATEWAYENDPOINT,
 });
 
-export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const connectionId = event.requestContext.connectionId as string;
+const responseOK = {
+  statusCode: 200,
+  body: "",
+};
 
+const responseForbidden = {
+  statusCode: 403,
+  body: "",
+};
+
+class HandlerError extends Error { }
+
+export const handle = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log("event:", event);
+  const connectionId = event.requestContext.connectionId as string;
   const routeKey = event.requestContext.routeKey as Action;
 
-  switch (routeKey) {
-    case "$connect":
-      return handleConnect(connectionId, event.queryStringParameters);
+  try {
+    switch (routeKey) {
+      case "$connect":
+        return handleConnect(connectionId, event.queryStringParameters);
+      case "$disconnect":
+        return handleDisconnect(connectionId);
+      case "getClients":
+        return handleGetClients(connectionId);
+      case "sendMessage":
+        return handleSendMessage(
+          await getClient(connectionId),
+          parseSendMessageBody(event.body),
+        );
+      case "getMessages":
+        return handleGetMessages(
+          await getClient(connectionId),
+          parseGetMessageBody(event.body),
+        );
+      default:
+        return responseForbidden;
+    }
+  } catch (e) {
+    if (e instanceof HandlerError) {
+      await postToConnection(
+        connectionId,
+        JSON.stringify({ type: "error", message: e.message }),
+      );
+      return responseOK;
+    }
 
-    case "$disconnect":
-      return handleDisconnect(connectionId);
-
-    case "getClients":
-      return handleGetClients(connectionId);
-
-    default:
-      return {
-        statusCode: 500,
-        body: "",
-      };
+    throw e;
   }
 };
 
 const handleConnect = async (
   connectionId: string,
   queryParameters: APIGatewayProxyEventQueryStringParameters | null,
-): Promise<APIGatewayProxyResult> => {
+) => {
   if (!queryParameters || !queryParameters["nickname"]) {
-    return {
-      statusCode: 403,
-      body: "",
-    };
+    return responseForbidden;
+  }
+
+  const existingConnectionId = await getConnectionIdByNickname(
+    queryParameters["nickname"],
+  );
+  if (
+    existingConnectionId &&
+    (await postToConnection(
+      existingConnectionId,
+      JSON.stringify({ type: "ping" }),
+    ))
+  ) {
+    return responseForbidden;
   }
 
   await docClient
@@ -54,29 +123,44 @@ const handleConnect = async (
     })
     .promise();
 
-  await notifyClients(connectionId);
+  await notifyClientChange(connectionId);
 
   return responseOK;
 };
 
-const handleDisconnect = async (connectionId: string): Promise<APIGatewayProxyResult> => {
-  await docClient
-    .delete({
+const notifyClientChange = async (excludedConnectionId: string) => {
+  const clients = await getAllClients();
+
+  await Promise.all(
+    clients.map(async (c) => {
+      if (excludedConnectionId === c.connectionId) {
+        return;
+      }
+
+      await postToConnection(
+        c.connectionId,
+        JSON.stringify({ type: "clients", value: clients }),
+      );
+    }),
+  );
+};
+
+const getAllClients = async (): Promise<Client[]> => {
+  const output = await docClient
+    .scan({
       TableName: CLIENTS_TABLE_NAME,
-      Key: {
-        connectionId,
-      },
     })
     .promise();
 
-  await notifyClients(connectionId);
+  const clients = output.Items || [];
 
-  return responseOK;
+  return clients as Client[];
 };
 
-const isConnectionNotExistError = (e: unknown) => (e as AWSError).statusCode === 410;
-
-const postToConnection = async (connectionId: string, messageBody: string): Promise<boolean> => {
+const postToConnection = async (
+  connectionId: string,
+  messageBody: string,
+): Promise<boolean> => {
   try {
     await apigw
       .postToConnection({
@@ -104,33 +188,174 @@ const postToConnection = async (connectionId: string, messageBody: string): Prom
   }
 };
 
-const getAllClients = async () => {
-  const output = await docClient
-    .scan({
+const isConnectionNotExistError = (e: unknown) =>
+  (e as AWSError).statusCode === 410;
+
+const handleDisconnect = async (connectionId: string) => {
+  await docClient
+    .delete({
       TableName: CLIENTS_TABLE_NAME,
+      Key: {
+        connectionId,
+      },
     })
     .promise();
 
-  const clients = output.Items ?? [];
-  return clients as Client[];
-};
-
-const handleGetClients = async (connectionId: string): Promise<APIGatewayProxyResult> => {
-  const clients = await getAllClients();
-  await postToConnection(connectionId, JSON.stringify({ type: "clients", value: clients }));
+  await notifyClientChange(connectionId);
 
   return responseOK;
 };
 
-const notifyClients = async (connnectionIdToExclude: string) => {
-  const clients = await getAllClients();
-  await Promise.all(
-    clients.map(async (c) => {
-      if (c.connectionId !== connnectionIdToExclude) {
-        return;
-      }
-
-      await postToConnection(c.connectionId, JSON.stringify({ type: "clients", value: clients }));
+const handleGetClients = async (connectionId: string) => {
+  await postToConnection(
+    connectionId,
+    JSON.stringify({
+      type: "clients",
+      value: await getAllClients(),
     }),
   );
+
+  return responseOK;
+};
+
+const handleSendMessage = async (client: Client, body: SendMessageBody) => {
+  const nicknameToNickname = getNicknameToNickname([
+    client.nickname,
+    body.recipientNickname,
+  ]);
+
+  await docClient
+    .put({
+      TableName: MESSAGES_TABLE_NAME,
+      Item: {
+        messageId: v4(),
+        nicknameToNickname,
+        message: body.message,
+        sender: client.nickname,
+        createdAt: new Date().getTime(),
+      },
+    })
+    .promise();
+
+  const recipientConnectionId = await getConnectionIdByNickname(
+    body.recipientNickname,
+  );
+
+  if (recipientConnectionId) {
+    await apigw
+      .postToConnection({
+        ConnectionId: recipientConnectionId,
+        Data: JSON.stringify({
+          type: "message",
+          value: {
+            sender: client.nickname,
+            message: body.message,
+          },
+        }),
+      })
+      .promise();
+  }
+
+  return responseOK;
+};
+
+const getClient = async (connectionId: string) => {
+  const output = await docClient
+    .get({
+      TableName: CLIENTS_TABLE_NAME,
+      Key: {
+        connectionId,
+      },
+    })
+    .promise();
+
+  if (!output.Item) {
+    throw new HandlerError("client does not exist");
+  }
+
+  return output.Item as Client;
+};
+
+const parseSendMessageBody = (body: string | null): SendMessageBody => {
+  const sendMsgBody = JSON.parse(body || "{}") as SendMessageBody;
+
+  if (!sendMsgBody || !sendMsgBody.recipientNickname || !sendMsgBody.message) {
+    throw new HandlerError("invalid SendMessageBody");
+  }
+
+  return sendMsgBody;
+};
+
+const getNicknameToNickname = (nicknames: string[]) =>
+  nicknames.sort().join("#");
+
+const getConnectionIdByNickname = async (
+  nickname: string,
+): Promise<string | undefined> => {
+  const output = await docClient
+    .query({
+      TableName: CLIENTS_TABLE_NAME,
+      IndexName: "NicknameIndex",
+      KeyConditionExpression: "#nickname = :nickname",
+      ExpressionAttributeNames: {
+        "#nickname": "nickname",
+      },
+      ExpressionAttributeValues: {
+        ":nickname": nickname,
+      },
+    })
+    .promise();
+
+  return output.Items && output.Items.length > 0
+    ? output.Items[0].connectionId
+    : undefined;
+};
+
+const handleGetMessages = async (client: Client, body: GetMessagesBody) => {
+  const output = await docClient
+    .query({
+      TableName: MESSAGES_TABLE_NAME,
+      IndexName: "NicknameToNicknameIndex",
+      KeyConditionExpression: "#nicknameToNickname = :nicknameToNickname",
+      ExpressionAttributeNames: {
+        "#nicknameToNickname": "nicknameToNickname",
+      },
+      ExpressionAttributeValues: {
+        ":nicknameToNickname": getNicknameToNickname([
+          client.nickname,
+          body.targetNickname,
+        ]),
+      },
+      Limit: body.limit,
+      ExclusiveStartKey: body.startKey,
+      ScanIndexForward: false,
+    })
+    .promise();
+
+  await postToConnection(
+    client.connectionId,
+    JSON.stringify({
+      type: "messages",
+      value: {
+        messages: output.Items && output.Items.length > 0 ? output.Items : [],
+        lastEvaluatedKey: output.LastEvaluatedKey,
+      },
+    }),
+  );
+
+  return responseOK;
+};
+
+const parseGetMessageBody = (body: string | null) => {
+  const getMessagesBody = JSON.parse(body || "{}") as GetMessagesBody;
+
+  if (
+    !getMessagesBody ||
+    !getMessagesBody.targetNickname ||
+    !getMessagesBody.limit
+  ) {
+    throw new HandlerError("invalid GetMessageBody");
+  }
+
+  return getMessagesBody;
 };
